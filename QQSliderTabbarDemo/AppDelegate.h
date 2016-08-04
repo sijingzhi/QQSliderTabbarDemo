83ddf319b9fab1852f914b540f940416ce27e425
lysss2324 <236647016@qq.com>
2016-08-04 19:25:38 +0800

    Initial Commit

---
+++ QQSliderTabbarDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QQSliderTabbarDemo
+//
+//  Created by JJT on 16/8/4.
+//  Copyright © 2016年 SJZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
